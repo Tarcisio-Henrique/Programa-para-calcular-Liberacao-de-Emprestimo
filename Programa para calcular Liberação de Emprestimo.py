4f856e3439a4e10c6a5e5c136845d3e9210a817a
Tarcisio Henrique <106704724+Tarcisio-Henrique@users.noreply.github.com>
2024-01-06 15:34:40 -0300

    Add files via upload

---
+++ Programa para calcular Liberação de Emprestimo.py
@@ -0,0 +1,16 @@
+# Programa que calcula aprovação de financiamento.
+
+casa = float(input('Qual o valor da casa desejada? R$'))
+salario = float(input('Qual o valor do seu salário? R$'))
+anos = int(input('Em quantos anos pretende pagar? '))
+prestacao  = casa / (anos * 12)
+parcelas = anos * 12
+minimo = salario * 30 / 100
+
+print('Para pagar uma casa no valor de R${:.2f} em {} anos'.format(casa, anos), end=' ')
+print('a prestação será de R${:.2f} em {} vezes.'.format(prestacao, parcelas))
+
+if prestacao <= minimo:
+    print('Emprestimo pode ser CONCEDIDO!')
+else:
+    print('Valor solicitado excedo o limite de 30% do salário. \nEmprestimo NEGADO!')
